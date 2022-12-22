111c3daad50d4b4eaae24401e7d4bedd97dbb782
pangyuteng <pteng@mednet.ucla.edu>
2022-12-22 13:41:51 -0800

    added caching logic

--- assess/ped-ct-seg/inference.sh
+++ assess/ped-ct-seg/inference.sh
@@ -2,5 +2,10 @@
 export image_file=$1
 export seg_folder=$2
 
+export liver_file=${seg_folder}/liver.nii.gz
+if [ -f ${liver_file} ]; then
+    echo "File found!"
+    exit 0
+fi
 # inference
 TotalSegmentator -i ${image_file} -o ${seg_folder}
