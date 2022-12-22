3b068a093422fa756c2c55f48be5d678a1a806f5
pangyuteng <pteng@mednet.ucla.edu>
2022-12-22 13:51:23 -0800

    updated caching logic

--- assess/ped-ct-seg/inference.sh
+++ assess/ped-ct-seg/inference.sh
@@ -4,7 +4,7 @@ export seg_folder=$2
 
 export file_count=$(ls ${seg_folder}/*.nii.gz | wc -l)
 if [ "${file_count}" -eq 104 ]; then
-    echo "All 104 files found, inference skipped!"
+    echo "All 104 files found, skipping inference!"
     exit 0
 fi
 
--- assess/ped-ct-seg/process.sh
+++ assess/ped-ct-seg/process.sh
@@ -5,4 +5,10 @@ export seg_folder=$3
 export seg_file=$4
 export score_file=$5
 
+if [ -f ${score_file} ]; then
+    echo "json file found, skipping process.py!"
+    exit 0
+fi
+
 python process.py $image_file $mask_file $seg_folder $seg_file $score_file
+
