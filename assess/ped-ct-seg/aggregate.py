94174d6ee7932407fd9f6c4242458f732c3c2b5a
pangyuteng <pteng@mednet.ucla.edu>
2022-12-22 14:44:11 -0800

    added aggregate.py

--- assess/ped-ct-seg/aggregate.py
+++ assess/ped-ct-seg/aggregate.py
@@ -2,22 +2,53 @@ import os
 import sys
 from pathlib import Path
 import json
+import numpy as np
 import pandas as pd
 
+agg_csv_file = 'agg.csv'
+summary_csv_file = 'summary.csv'
 def main(root_folder):
-    json_file_list = []
-    for path in Path(root_folder).rglob("*scores.json"):
-        json_file_list.append(str(path))
 
-    mylist = []
-    for x in json_file_list:
-        print(x)
-        with open(x,'r') as f:
-            scores_dict = json.loads(f.read())
-        mylist.append(scores_dict['dice'])
+    if not os.path.exists(agg_csv_file):
+        json_file_list = []
+        for path in Path(root_folder).rglob("*scores.json"):
+            json_file_list.append(str(path))
 
-    df = pd.DataFrame(mylist)
-    df.to_csv('agg.csv',index=False)
+        mylist = []
+        for x in json_file_list:
+            with open(x,'r') as f:
+                scores_dict = json.loads(f.read())
+            mylist.append(scores_dict['dice'])
+
+        df = pd.DataFrame(mylist)
+        df.to_csv(agg_csv_file,index=False)
+    else:
+        df = pd.read_csv(agg_csv_file)
+
+    if not os.path.exists(summary_csv_file):
+        mylist = []
+        for organ_name in df.columns:
+            myitem=dict(
+                organ_name=organ_name,
+            )
+            val_list = df[organ_name].dropna()
+            if len(val_list)>0:
+                n = len(val_list)
+                m,sd = np.mean(val_list),np.std(val_list)
+                med = np.median(val_list)
+                myitem.update(dict(
+                    val_median=np.round(med,4),
+                    val_mean=np.round(m,4),
+                    val_sd=np.round(sd,4),
+                    val_n=n,
+                ))
+            mylist.append(myitem)
+        df = pd.DataFrame(mylist)
+        df.to_csv(summary_csv_file,index=False)
+    dtype_dict = {'val_mean':"Float32",'val_sd':"Float32",'val_n':"Int64"}
+    df = pd.read_csv(summary_csv_file,dtype=dtype_dict)
+    print(df)
+
 
 if __name__ == "__main__":
     # root folder
