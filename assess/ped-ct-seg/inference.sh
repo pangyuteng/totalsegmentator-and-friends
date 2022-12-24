b2779335edd372d6adc7c364a8c9e91f60021e55
pangyuteng <pangyuteng@gmail.com>
2022-12-24 20:24:33 +0000

    fixed inference.sh

--- assess/ped-ct-seg/inference.sh
+++ assess/ped-ct-seg/inference.sh
@@ -3,9 +3,11 @@ export image_file=$1
 export seg_folder=$2
 
 export file_count=$(ls ${seg_folder}/*.nii.gz | wc -l)
-if [ "${file_count}" -eq 104 ]; then
-    echo "All 104 files found, skipping inference!"
-    exit 0
+if [ -f "${seg_folder}" ]; then
+    if [ "${file_count}" -eq 104 ]; then
+        echo "All 104 files found, skipping inference!"
+        exit 0
+    fi
 fi
 
 # inference
