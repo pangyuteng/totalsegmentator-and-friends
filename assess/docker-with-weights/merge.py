5e29cd8788f020d78bcd228fe78ccfb57143cfb8
pangyuteng <pangyuteng@gmail.com>
2023-03-28 03:32:48 +0000

    added task 273 for body

--- assess/docker-with-weights/merge.py
+++ assess/docker-with-weights/merge.py
@@ -2,11 +2,11 @@ import os
 import sys
 import numpy as np
 import SimpleITK as sitk
-
-input_seg_folder = sys.argv[1]
-output_nifti_file = sys.argv[2]
-
-
 from totalsegmentator.libs import combine_masks_to_multilabel_file
 
-combine_masks_to_multilabel_file(input_seg_folder,output_nifti_file)
+if __name__ == "__main__":
+    input_seg_folder = sys.argv[1]
+    output_nifti_file = sys.argv[2]
+    combine_masks_to_multilabel_file(input_seg_folder,output_nifti_file)
+
+
